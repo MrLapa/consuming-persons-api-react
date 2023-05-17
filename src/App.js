bd6d3a008b74ad6edca27cd11af9ecdf13e102e1
Carlos Garcia <carlos.garcia@boatyardx.com>
2023-05-17 17:17:27 -0500

    tweak App.js file based on the persons api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+import "./App.css";
+import PersonsList from "./person/PersonsList";
+
+//Using fetch API without third party libraries
+const ALL_PERSONS = gql`
+  query AllPersons {
+    allPersons {
+      name
+      phone
+      address {
+        street
+        city
+      }
+      id
+    }
+  }
+`;
 
 function App() {
+  const { data, loading, error } = useQuery(ALL_PERSONS);
+
+  if (error) return <p>Error: {error.message}</p>;
+  if (loading) return <p>Loading...</p>;
+  if (!loading && data.allPersons.length === 0) return <p>Found no movies</p>;
+
+  console.log("message");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <h1>GraphQL Persons</h1>
+      </section>
+      <section>
+        <h1>GraphQL Persons Apollo Provider</h1>
+      </section>
+      <section>
+        {!loading && data.allPersons.length > 0 && (
+          <PersonsList movies={data.allPersons} />
+        )}
+      </section>
+    </React.Fragment>
   );
 }
 
