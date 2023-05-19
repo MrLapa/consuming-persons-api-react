d954ce2b51bd3e39355e0bb7e7916ddfcb966fdf
Carlos Garcia <carlos.garcia@boatyardx.com>
2023-05-18 21:41:49 -0500

    consuming a graphql query to get a single object and triggering the query on submit event

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { gql, useQuery } from "@apollo/client";
 
 import "./App.css";
@@ -19,17 +19,73 @@ const ALL_PERSONS = gql`
   }
 `;
 
+const GET_PERSON = gql`
+  query Query($name: String!) {
+    findPerson(name: $name) {
+      name
+      phone
+      address {
+        street
+        city
+      }
+      id
+    }
+  }
+`;
+
 function App() {
   const { data, loading, error } = useQuery(ALL_PERSONS);
+  const [inputValue, setInputValue] = useState("");
+  const [isInputDisabled, setIsInputDisabled] = useState(false);
+  const [skipValue, setSkipValue] = useState(true);
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+    setSkipValue(true);
+  };
+
+  const {
+    loading: loadingPerson,
+    error: errorPerson,
+    data: dataPerson,
+    refetch: refetchPerson,
+  } = useQuery(GET_PERSON, {
+    skip: skipValue, // Skip initial automatic fetch
+    variables: { name: inputValue },
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setSkipValue(false);
+    setIsInputDisabled(true);
+    await refetchPerson();
+    setIsInputDisabled(false);
+  };
 
   if (error) return <p>Error: {error.message}</p>;
   if (loading) return <p>Loading...</p>;
   if (!loading && data.allPersons.length === 0) return <p>Found no movies</p>;
 
-  console.log("message");
-
   return (
     <React.Fragment>
+      <div>
+        <h2>Find a person</h2>
+        <form onSubmit={handleSubmit}>
+          <input disabled={isInputDisabled} onChange={handleChange}></input>
+          <button type="submit" disabled={isInputDisabled}>
+            {loadingPerson ? "Loading..." : "Submit"}
+          </button>
+        </form>
+        {errorPerson && <p>Error: {errorPerson.message}</p>}
+        {!loadingPerson && dataPerson && dataPerson.findPerson && (
+          <ul>
+            <li>Name:{dataPerson.findPerson.name}</li>
+            <li>Phone: {dataPerson.findPerson.phone}</li>
+            <li>Street: {dataPerson.findPerson.address.street}</li>
+            <li>City: {dataPerson.findPerson.address.city}</li>
+          </ul>
+        )}
+      </div>
       <section>
         <h1>GraphQL Persons</h1>
       </section>
