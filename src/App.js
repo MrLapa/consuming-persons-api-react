a7b40c3a9ece9b2cfac0bb96e8a1a029cbb3843f
Carlos Garcia <carlos.garcia@boatyardx.com>
2023-05-20 10:09:28 -0500

    consuming a mutation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { gql, useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 
 import "./App.css";
 import PersonsList from "./person/PersonsList";
@@ -33,17 +33,35 @@ const GET_PERSON = gql`
   }
 `;
 
+const ADD_PERSON_MUTATION = gql`
+  mutation AddPerson(
+    $name: String!
+    $phone: String!
+    $street: String!
+    $city: String!
+  ) {
+    addPerson(name: $name, phone: $phone, street: $street, city: $city) {
+      name
+      phone
+      address {
+        street
+        city
+      }
+      id
+    }
+  }
+`;
+
 function App() {
-  const { data, loading, error } = useQuery(ALL_PERSONS);
   const [inputValue, setInputValue] = useState("");
   const [isInputDisabled, setIsInputDisabled] = useState(false);
   const [skipValue, setSkipValue] = useState(true);
+  const [inputName, setInputName] = useState("");
+  const [inputPhone, setInputPhone] = useState("");
+  const [inputStreet, setInputStreet] = useState("");
+  const [inputCity, setInputCity] = useState("");
 
-  const handleChange = (event) => {
-    setInputValue(event.target.value);
-    setSkipValue(true);
-  };
-
+  const { data, loading, error } = useQuery(ALL_PERSONS);
   const {
     loading: loadingPerson,
     error: errorPerson,
@@ -53,6 +71,31 @@ function App() {
     skip: skipValue, // Skip initial automatic fetch
     variables: { name: inputValue },
   });
+  const [
+    addPerson,
+    { data: dataNewPerson, loading: loadingNewPerson, error: errorNewPerson },
+  ] = useMutation(ADD_PERSON_MUTATION);
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+    setSkipValue(true);
+  };
+
+  const handleChangeName = (event) => {
+    setInputName(event.target.value);
+  };
+
+  const handleChangePhone = (event) => {
+    setInputPhone(event.target.value);
+  };
+
+  const handleChangeStreet = (event) => {
+    setInputStreet(event.target.value);
+  };
+
+  const handleChangeCity = (event) => {
+    setInputCity(event.target.value);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -60,6 +103,27 @@ function App() {
     setIsInputDisabled(true);
     await refetchPerson();
     setIsInputDisabled(false);
+  };
+
+  const handleAddPersonSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log("entradaaaaa");
+
+    try {
+      const { data: newPerson } = await addPerson({
+        variables: {
+          name: inputName,
+          phone: inputPhone,
+          street: inputStreet,
+          city: inputCity,
+        },
+      });
+
+      console.log("newPerson", newPerson);
+    } catch (error) {
+      console.log("error", error);
+    }
   };
 
   if (error) return <p>Error: {error.message}</p>;
@@ -79,6 +143,52 @@ function App() {
         {errorPerson && <p>Error: {errorPerson.message}</p>}
         {!loadingPerson && dataPerson && dataPerson.findPerson && (
           <ul>
+            <li>Name:{dataPerson.findPerson.name}</li>
+            <li>Phone: {dataPerson.findPerson.phone}</li>
+            <li>Street: {dataPerson.findPerson.address.street}</li>
+            <li>City: {dataPerson.findPerson.address.city}</li>
+          </ul>
+        )}
+      </div>
+      <div>
+        <h2>Add a person</h2>
+        <form onSubmit={handleAddPersonSubmit}>
+          <div>
+            <label>Name: </label>
+            <input
+              disabled={isInputDisabled}
+              onChange={handleChangeName}
+            ></input>
+          </div>
+          <div>
+            <label>Phone: </label>
+            <input
+              disabled={isInputDisabled}
+              onChange={handleChangePhone}
+            ></input>
+          </div>
+          <div>
+            <label>Street: </label>
+            <input
+              disabled={isInputDisabled}
+              onChange={handleChangeStreet}
+            ></input>
+          </div>
+          <div>
+            <label>City: </label>
+            <input
+              disabled={isInputDisabled}
+              onChange={handleChangeCity}
+            ></input>
+          </div>
+          <button type="submit" disabled={loadingNewPerson}>
+            {loadingNewPerson ? "Loading..." : "Add person"}
+          </button>
+        </form>
+        {errorPerson && <p>Error: {errorPerson.message}</p>}
+        {!loadingPerson && dataPerson && dataPerson.findPerson && (
+          <ul>
+            <li>Name:{dataPerson.findPerson.id}</li>
             <li>Name:{dataPerson.findPerson.name}</li>
             <li>Phone: {dataPerson.findPerson.phone}</li>
             <li>Street: {dataPerson.findPerson.address.street}</li>
